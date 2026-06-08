2b806a9f6e5dd3f4a112bca7dee8b0be2236174e
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2026-06-08 10:01:15 -0500

    Uhh...

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("A", {
-    name: "Addition", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("H", {
+    name: "Hydrogen", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "+", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "rgb(255, 22, 80)",
+    color: "rgb(226, 255, 253)",
     requires: new Decimal(1), // Can be a function that takes requirement increases into account
-    resource: "Addition Points", // Name of prestige currency
+    resource: "Hydroen", // Name of prestige currency
     baseResource: "Numbers", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
