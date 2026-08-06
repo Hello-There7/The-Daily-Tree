073e0967ba6fcf2cef0e2384d646fb7633d1c7db
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2026-08-06 15:02:28 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,17 +1,17 @@
 addLayer("H", {
     name: "Hydrogen", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "+", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "H", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "rgb(226, 255, 253)",
+    color: "rgb(255, 255, 255)",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Hydrogen", // Name of prestige currency
-    baseResource: "Neutrons", // Name of resource prestige is based on
+    baseResource: "Electrons", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
@@ -21,25 +21,30 @@ addLayer("H", {
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "a", description: "A: Reset for Addition Points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
     layerShown(){return true},
     upgrades: {
         11: {
-            title: "+1",
-            description: "+1 Numbers/sec",
+            title: "Protium",
+            description: "Double Electron gain",
             cost: new Decimal(1),
         },
         12: {
-            title: "+2",
-            description: "+2 Numbers/sec",
+            title: "Deuterium",
+            description: "Hydrogen boosts Electron gain",
             cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
         13: {
-            title: "+3",
-            description: "+3 Numbers/sec",
+            title: "Tritium",
+            description: "Electrons boost Electron Gain",
             cost: new Decimal(3),
+            effect() {
+                return player.points.add(1).pow(0.1)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
         },
     },
 })
